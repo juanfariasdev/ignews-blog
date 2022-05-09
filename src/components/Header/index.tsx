8b8f7356020b4ccbfd43af889900cd3ae0fa439c
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2022-05-09 09:14:15 -0300

    Logo linkada com a home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import { SignInButton } from "../SignInButton"
 
 import styles from './styles.module.scss'
@@ -7,7 +8,7 @@ function Header(){
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <Image src="/images/logo.svg" alt= "Logo IG.News" width='100%' height='100%'/>
+                <Link href={"/"}><a><Image src="/images/logo.svg" alt= "Logo IG.News" width='100%' height='100%'/></a></Link>
                 <nav>
                 <a className={styles.active}>Home</a>
                 <a>Posts</a>
