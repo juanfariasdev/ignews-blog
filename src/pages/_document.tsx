ad0418e992f93f6cd7b451d5b51e4cc02e48f3c6
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2022-05-10 09:46:18 -0300

    Fonte: Source Sans Pro adicionado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,10 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/> 
+ 
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&family=Source+Sans+Pro:wght@400;700;900&display=swap" rel="stylesheet" /> 
                     <link rel="shortcut icon" href="/favicon.png" type="image/png" />
                 </Head>
                 <body>
