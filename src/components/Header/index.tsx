6b1c8a34ae86299db65cbc60dc73707d70b7ddbe
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2022-05-11 11:11:59 -0300

    melhorias de layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,10 +10,16 @@ function Header(){
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <Link href={"/"}><a><Image src="/images/logo.svg" alt= "Logo IG.News" width='150px' height='300px' layout="raw"/></a></Link>
+                <Link href={"/"}><a><Image src="/images/logo.svg" alt= "Logo IG.News" width='150px' height='50px' layout="raw"/></a></Link>
                 <nav>
-                    <ActiveLink activeClassName={styles.active} href={"/"} prefetch={true}><a>Home</a></ActiveLink>
-                    <ActiveLink activeClassName={styles.active}  href={"/posts"} prefetch={true}><a>Posts</a></ActiveLink>
+                    <ul className={styles.nav_menu}>
+                        <li>
+                            <ActiveLink activeClassName={styles.active} href={"/"} prefetch={true}><a>Home</a></ActiveLink>
+                        </li>
+                        <li>
+                            <ActiveLink activeClassName={styles.active}  href={"/posts"} prefetch={true}><a>Posts</a></ActiveLink>
+                        </li>
+                    </ul>
                 </nav>
 
             <SignInButton />
